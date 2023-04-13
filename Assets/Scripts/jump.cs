1342e65a4919870a1aae0307c94891f2d4b3053d
VisionaRnn <843931848@qq.com>
2023-04-13 19:08:39 +0800

    提交信息

---
+++ Assets/Scripts/jump.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class jump : MonoBehaviour {
+	
+	public void Jump (string path){
+		SceneManager.LoadScene(path);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
